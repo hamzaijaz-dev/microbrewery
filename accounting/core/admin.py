9bf672bc72f5e779876fcf2f3b96e236b264682c
Hamza Ijaz <24452313+hamzaijaz-dev@users.noreply.github.com>
2023-09-13 13:18:16 +0500

    updated readme

--- accounting/core/admin.py
+++ accounting/core/admin.py
@@ -1,4 +1,7 @@
-from core.models import Revenue
+from core.models import ExpenseType, Expense, Revenue, RevenueType
 from django.contrib import admin
 
+admin.site.register(RevenueType)
 admin.site.register(Revenue)
+admin.site.register(ExpenseType)
+admin.site.register(Expense)
